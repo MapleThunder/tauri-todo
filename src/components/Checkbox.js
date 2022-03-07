d31e23fd9cfc14724386c69d20965bbbcd47989c
Niko Bentley <niko.bentley@redspace.com>
2022-03-07 14:17:59 -0400

    gitignore and spelling update

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -11,7 +11,7 @@ export function Checkbox({ id }) {
       data-testid="checkbox-action"
       onClick={() => archiveTask()}
     >
-      <span className="chackbox" />
+      <span className="checkbox" />
     </div>
   );
 }
