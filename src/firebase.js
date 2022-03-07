04737e937a86af111d6409d90ed9e302f30550b7
Niko Bentley <niko.bentley@redspace.com>
2022-03-07 10:32:39 -0400

    updating firebase configs to read from .env files correctly

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import "dotenv/config";
-import firebase from "firebase/app";
+// import "dotenv/config";
+import { initializeApp } from "firebase/app";
 import "firebase/firestore";
 
-const firebaseConfig = firebase.initializeApp({
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseUrl: process.env.databaseUrl,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+const firebaseConfig = initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseUrl: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 export { firebaseConfig as firebase };
